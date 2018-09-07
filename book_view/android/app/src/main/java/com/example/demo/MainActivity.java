806d6bf82d91e518d1819bd5affda5fcd8579451
StrongX <strongxlx@gmail.com>
2018-09-07 17:40:01 +0800

    完成了安卓adnet平台代码的嵌入

--- book_view/android/app/src/main/java/com/example/demo/MainActivity.java
+++ book_view/android/app/src/main/java/com/example/demo/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.demo;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "xlx.flutter.io/adnet";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+            new MethodCallHandler() {
+              @Override
+              public void onMethodCall(MethodCall call, Result result) {
+                // TODO
+                if (call.method.equals("loadInterstitialObj")) {
+
+                } else if(call.method.equals("showInterstitialObj")){
+                  
+                }else{
+                }
+              }
+            });
   }
 }

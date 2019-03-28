54e69a1107ba2c9f6ed902f4098a5d73d1b589d5
StrongX <strongxlx@gmail.com>
2019-03-28 17:43:36 +0800

    no message

--- fixAuthor.sh
+++ fixAuthor.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-git filter-branch --env-filter
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
-if [ "$GIT_COMMITTER_EMAIL" = "385135499@qq.com" ]
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="385135499@qq.com"
+CORRECT_NAME="StrongX"
+CORRECT_EMAIL="strongxlx@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="StrongX"
-    cm="strongxlx@gmail.com"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "385135499@qq.com" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    an="StrongX"
-    am="strongxlx@gmail.com"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-    export GIT_AUTHOR_NAME="$an"
-    export GIT_AUTHOR_EMAIL="$am"
-    export GIT_COMMITTER_NAME="$cn"
-    export GIT_COMMITTER_EMAIL="$cm"
+' --tag-name-filter cat -- --branches --tags
